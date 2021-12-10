fdbb4c9203bf37e5435d676c66f59f9fc93fabee
Pedro Lucas <pedrowalmart10@gmail.com>
2021-12-10 17:57:14 -0300

    Add login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import style from "./app.module.scss";
 import { Login } from "./components/login";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { NavBar } from "./components/navbar";
 import { Darshboard } from "./components/darshboard";
+import { useRoutes } from "hookrouter";
 
 function App() {
+	const id = window.localStorage.getItem("@user:id");
+
+	const routes = {
+    '/' :()=><Login/>,
+    '/dashboard' :()=> <Darshboard/>,
+   };
+  const routeResults = useRoutes(routes);
+
+	/*let navigate = useNavigate();*/
 
 	return (
-		<div>
-			<Router>
-				<NavBar />
-				<Routes>
-					<Route path="/" element={<Login />}/>
-					<Route path="/dashboard" element={<Darshboard />}/>
-				</Routes>
-			</Router>
-		</div>
+		<div className="App">
+			{!id ? '' /*navigate("/login")*/ : <NavBar />}
+			{routeResults||<h1>PAGE  NOT FOUND</h1>}
+			{/*<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/" element={<Darshboard />} />
+				<Route path="/dashboard" element={<Darshboard />} />
+			</Routes>*/}
+		</div >
 	);
 }
 
--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,8 +1,38 @@
+import { useNavigate } from "@reach/router";
+import { FormEvent, useState } from "react";
+import { AuthProvider } from "../../contexts/auth";
+import { api } from "../../service/api";
 import style from "../login/login.module.scss";
 
 export function Login() {
-	function buttonClick() {
-		window.location.href="/dashboard";
+	const navigate = useNavigate();
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	async function login(event: FormEvent) {
+		event.preventDefault();
+
+		if (!email.trim()) {
+			alert("Preencha o email");
+		} else if (!password) {
+			alert("Preencha sua senha");
+		}
+
+		const user = await api.post("login", {
+			email: email,
+      password: password
+		});
+
+		if (!user.data) {
+			alert("Usuario não cadastrado");
+		} else {
+			navigate('/dashboard');
+			localStorage.setItem("@user:id", user.data.id);
+		}
+
+		setEmail("");
+		setPassword("");
 	}
 
 	return (
@@ -20,7 +50,13 @@ export function Login() {
 								src="https://cdn-icons-png.flaticon.com/512/747/747376.png"
 								alt="user"
 							/>
-							<input name="login" placeholder="Login" />
+							<input
+								name="email"
+								id="email"
+								onChange={(event) => setEmail(event.target.value)}
+								value={email}
+								placeholder="Email"
+							/>
 						</span>
 					</li>
 					<li>
@@ -29,12 +65,20 @@ export function Login() {
 								src="https://cdn-icons.flaticon.com/png/512/2901/premium/2901185.png?token=exp=1638293312~hmac=0880a8aab8797eedaa02ea523a2c2f0c"
 								alt="password"
 							/>
-							<input name="senha" placeholder="Senha" />
+							<input
+								name="senha"
+								id="senha"
+								onChange={(event) => setPassword(event.target.value)}
+								value={password}
+								placeholder="Senha"
+							/>
 						</span>
 					</li>
 					<li>
 						<a href="#">Register</a> <a href="#">Forgot Password?</a>{" "}
-						<button type="submit" >Sign In</button>
+						<button type="submit" onClick={login}>
+							Sign In
+						</button>
 					</li>
 				</ul>
 			</form>
